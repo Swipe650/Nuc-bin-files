8d8512834f35fb0293c598e5f3c378ac36612c67
Swipe <swipe650@gmail.com>
2018-08-12 18:28:45 +0100

    gave kde connect priority order

--- firewall.sh
+++ firewall.sh
@@ -3,10 +3,10 @@
 sudo ufw reset
 sudo ufw default deny incoming
 sudo ufw default deny outgoing
-sudo ufw allow out on tun0 from any to any
-sudo ufw allow in on tun0 from any to any
 sudo ufw allow 1714:1764/udp
 sudo ufw allow 1714:1764/tcp
+sudo ufw allow out on tun0 from any to any
+sudo ufw allow in on tun0 from any to any
 sudo ufw enable
 
 if [ -e /home/swipe/.conky/fwoff.png ] 
