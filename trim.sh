80076395a015b819c51eb3025f093b740c363d16
Swipe <swipe650@gmail.com>
2020-01-27 19:39:30 +0000

    trim from fstab

--- trim.sh
+++ trim.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 LOG=/var/log/trim.log
 echo "*** $(date -R) ***" >> $LOG
-fstrim -v /boot >> $LOG
-fstrim -v / >> $LOG
-fstrim -v /home >> $LOG
+
+#fstrim -v /boot >> $LOG
+#fstrim -v / >> $LOG
+#fstrim -v /home >> $LOG
+
+# trim all supported mounted filesystems from /etc/fstab
+sudo fstrim --fstab --verbose >> $LOG
