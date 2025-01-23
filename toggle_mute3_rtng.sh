fe85589ac1421579545955b9baead17f40a67fa7
Swipe <swipe650@gmail.com>
2025-01-23 22:37:37 +0000

    simplfied code

--- toggle_mute3_rtng.sh
+++ toggle_mute3_rtng.sh
@@ -1,133 +1,98 @@
 #!/bin/bash
 
-mute () { "$HOME/bin/mute_radiotray-ng" -m /usr/bin/radiotray-ng && rename_xmuted; }
-unmute () { "$HOME/bin/mute_radiotray-ng" -u /usr/bin/radiotray-ng && rename_muted; }
-mute_vlc () { "$HOME/bin/mute_radiotray-ng" -m /usr/bin/vlc; }
-unmute_vlc () { "$HOME/bin/mute_radiotray-ng" -u /usr/bin/vlc; }
-rename_xmuted () { if [ -f "$HOME/.conky/xmuted.png" ]; then { mv "$HOME/.conky/xmuted.png" "$HOME/.conky/muted.png"; } fi }
-rename_muted () { if [ -f "$HOME/.conky/muted.png" ]; then { mv "$HOME/.conky/muted.png" "$HOME/.conky/xmuted.png"; } fi }
+# Generic mute/unmute function
+mute_app() { "$HOME/bin/mute_radiotray-ng" -m "$1" && rename_muted_file; } 
+unmute_app() { "$HOME/bin/mute_radiotray-ng" -u "$1" && rename_unmuted_file; } 
+
+rename_muted_file() {
+    if [ -f "$HOME/.conky/xmuted.png" ]; then
+        mv "$HOME/.conky/xmuted.png" "$HOME/.conky/muted.png"
+    fi
+}
+
+rename_unmuted_file() {
+    if [ -f "$HOME/.conky/muted.png" ]; then
+        mv "$HOME/.conky/muted.png" "$HOME/.conky/xmuted.png"
+    fi
+}
 
 # Conky countdown timer
-conkytimer()
-{
-   sec=$1
-  
-   for (( i = 0; i < sec; i++ )); do
-         # write remaining sec to file
-         timer=$((sec-i))
-         display="                  ${timer}"
-         echo "$display" > ~/.conkytimer
-         # wait 1sec
-         sleep 1
-   done
-
-   if [[ -f ~/.conkytimer ]]; then rm ~/.conkytimer & touch ~/.conkytimer
-   fi
+conkytimer() {
+    sec=$1
+    for (( i = 0; i < sec; i++ )); do
+        timer=$((sec-i))
+        echo "                  ${timer}" > ~/.conkytimer
+        sleep 1
+    done
+    rm -f ~/.conkytimer
+    touch ~/.conkytimer
 }
 
 # Get current volume level and show OSD dialog
-show_osd_dialog()
-{
-   vollevel=$(amixer -D pulse get Master | awk -F 'Left:|[][]' 'BEGIN {RS=""}{ print $3 }')
-
-   # Show dialog and remove "%" character from $vollevel
-   qdbus org.kde.plasmashell /org/kde/osdService org.kde.osdService.volumeChanged "${vollevel::-1}"
+show_osd_dialog() {
+    vollevel=$(amixer -D pulse get Master | awk -F 'Left:|[][]' 'BEGIN {RS=""}{ print $3 }')
+    qdbus org.kde.plasmashell /org/kde/osdService org.kde.osdService.volumeChanged "${vollevel::-1}"
 }
 
-# top of the hour dialog
-top_of_the_hour_dialog()
-      {
-        mute
+# Mute/unmute actions and top-of-the-hour dialog
+top_of_the_hour_dialog() {
+    mute_app /usr/bin/radiotray-ng
+    mute_app /usr/bin/vlc
+    qdbus org.kde.plasmashell /org/kde/osdService org.kde.osdService.volumeChanged 0
+    conkytimer "$adlength"
+    unmute_app /usr/bin/radiotray-ng
+    unmute_app /usr/bin/vlc
+    show_osd_dialog
+}
 
-        qdbus org.kde.plasmashell /org/kde/osdService org.kde.osdService.volumeChanged 0
-        
-        conkytimer "$adlength"
-          
-        unmute
-
-        show_osd_dialog
-
-        exit
-      }
-
+# Check top of the hour conditions
 check_top_of_the_hour() {
-    # Get current time in minutes
     currenttime=$(date +%M)
-
-    # Define station-time mappings and corresponding adlength values
     stations=(
         ".tr:TalkRadio:01 02 03 04:50"
         ".tr:TalkRadio:29 30 31 32 33 34 35 36 37:170"
         ".lbc:LBC UK:00 01 02 03 04 05 06:30"
     )
 
-    # Loop through stations and check time for each IFS=Station Information Array
     for station in "${stations[@]}"; do
         IFS=':' read -r file station_name times adlength_value <<< "$station"
-        
-        # Check if the station file exists
-        if test -f "$file"; then
-            # Check if currenttime matches any of the defined times for this station
-            if [[ " $times " =~ " $currenttime " ]]; then
-                declare -i adlength=$adlength_value
-                top_of_the_hour_dialog
-            fi
+        if test -f "$file" && [[ " $times " =~ " $currenttime " ]]; then
+            adlength=$adlength_value
+            top_of_the_hour_dialog
         fi
     done
 }
 
-
-check_top_of_the_hour
-
-mute
-mute_vlc
-
-qdbus org.kde.plasmashell /org/kde/osdService org.kde.osdService.volumeChanged 0
-
-
-check_for_off_peak() 
-{
-    # Get current time in minutes
+# Check if off-peak period
+check_for_off_peak() {
     currenttime=$(date +%H%M)
-   
-    # Define station-time pairs
     stations=(".lbc:LBC UK:120" ".tr:TalkRadio:170")
 
-    # Loop through stations and check time for each
     for station in "${stations[@]}"; do
         IFS=':' read -r file station_name timeout_value <<< "$station"
-        
-        if test -f "$file"; then
-            if [ "$currenttime" -gt "1900" ] || [ "$currenttime" -lt "0600" ]; then
-                timeout=$timeout_value
-            fi
+        if test -f "$file" && { [ "$currenttime" -gt "1900" ] || [ "$currenttime" -lt "0600" ]; }; then
+            timeout=$timeout_value
         fi
     done
 }
 
-
-# Function for default adbreak length
-default_adbreak_length() 
-{
-   # Sets timeout value for function
-   timeout=180  # Set value for adbreak length
-
-   # Run check_for_off_peak()
-   check_for_off_peak
-
-   # Wait until $SECONDS is greater than $timeout then execute the loop
-   while [ "$SECONDS" -le "$timeout" ]; do
-      # Display remaining time
-      echo "                  $((timeout - SECONDS))" > ~/.conkytimer
-      sleep 2
-   done
+# Default adbreak length function
+default_adbreak_length() {
+    timeout=180
+    check_for_off_peak
+    while [ "$SECONDS" -le "$timeout" ]; do
+        echo "                  $((timeout - SECONDS))" > ~/.conkytimer
+        sleep 2
+    done
 }
 
+# Main script
+check_top_of_the_hour
+mute_app /usr/bin/radiotray-ng
+mute_app /usr/bin/vlc
+qdbus org.kde.plasmashell /org/kde/osdService org.kde.osdService.volumeChanged 0
 default_adbreak_length
-
 conkytimer "$seconds"
-
-unmute
-unmute_vlc
-
+unmute_app /usr/bin/radiotray-ng
+unmute_app /usr/bin/vlc
 show_osd_dialog
