a6d63e34492ed8178902a790b2a28a9895829778
Swipe <swipe650@gmail.com>
2026-03-17 23:14:21 +0000

    consolidated version

---
+++ consolidated_toggle_mute_rtng.sh
@@ -0,0 +1,175 @@
+#!/bin/bash
+
+CONKY_DIR="$HOME/.conky"
+TIMER_FILE="$HOME/.conkytimer"
+
+###############################################################################
+# GET RADIOTRAY STATE (single query)
+###############################################################################
+
+RT_STATE=$(qdbus com.github.radiotray_ng /com/github/radiotray_ng \
+com.github.radiotray_ng.get_player_state)
+
+st=$(jq -r '.station' <<< "$RT_STATE")
+muted=$(jq -r '.mute' <<< "$RT_STATE")
+
+###############################################################################
+# GET PIPEWIRE STREAM
+###############################################################################
+
+radiotray_stream() {
+    wpctl status | awk '
+        /Streams:/ {f=1; next}
+        f && /radiotray-ng/ {print $1; exit}
+    ' | tr -d '.'
+}
+
+###############################################################################
+# ICON CONTROL
+###############################################################################
+
+toggle_icon() {
+
+    case "$1" in
+        muted)
+            [[ -f "$CONKY_DIR/xmuted.png" ]] && \
+            mv "$CONKY_DIR/xmuted.png" "$CONKY_DIR/muted.png"
+        ;;
+        unmuted)
+            [[ -f "$CONKY_DIR/muted.png" ]] && \
+            mv "$CONKY_DIR/muted.png" "$CONKY_DIR/xmuted.png"
+        ;;
+    esac
+}
+
+###############################################################################
+# MUTE CONTROL
+###############################################################################
+
+mute_app() {
+
+    [[ "$muted" == "true" ]] && return
+
+    stream=$(radiotray_stream)
+    [[ -z "$stream" ]] && return
+
+    wpctl set-mute "$stream" toggle
+    toggle_icon muted
+}
+
+unmute_app() {
+
+    [[ "$muted" == "false" ]] && return
+
+    stream=$(radiotray_stream)
+    [[ -z "$stream" ]] && return
+
+    wpctl set-mute "$stream" toggle
+    toggle_icon unmuted
+}
+
+###############################################################################
+# KDE OSD
+###############################################################################
+
+show_osd() {
+
+    vol=$(amixer -D pulse get Master \
+        | awk -F 'Left:|[][]' 'BEGIN{RS=""}{print $3}')
+
+    qdbus org.kde.plasmashell /org/kde/osdService \
+    org.kde.osdService.volumeChanged "${vol::-1}"
+}
+
+show_muted_osd() {
+
+    qdbus org.kde.plasmashell /org/kde/osdService \
+    org.kde.osdService.volumeChanged 0
+}
+
+###############################################################################
+# CONKY TIMER
+###############################################################################
+
+conky_timer() {
+
+    local seconds=$1
+
+    for ((i=seconds;i>0;i--)); do
+        printf "                  %s\n" "$i" > "$TIMER_FILE"
+        sleep 1
+    done
+
+    : > "$TIMER_FILE"
+}
+
+###############################################################################
+# TOP OF HOUR ADBREAK
+###############################################################################
+
+top_of_hour_adbreak() {
+
+    mute_app
+    show_muted_osd
+
+    conky_timer "$1"
+
+    unmute_app
+    show_osd
+    exit
+}
+
+check_top_of_hour() {
+
+    minute=$(date +%M)
+
+    stations=(
+        ".tr:01 02 03 04:50"
+        ".tr:28 29 30 31 32 33 34 35 36 37:150"
+        ".lbc:00 01 02 03 04 05 06:30"
+    )
+
+    for s in "${stations[@]}"; do
+
+        IFS=':' read -r file times length <<< "$s"
+
+        if [[ -f "$file" && " $times " =~ " $minute " ]]; then
+            top_of_hour_adbreak "$length"
+        fi
+    done
+}
+
+###############################################################################
+# DEFAULT ADBREAK
+###############################################################################
+
+default_adbreak() {
+
+    if [[ $st == "GB News" ]]; then
+        timeout=225
+    else
+        timeout=185
+    fi
+
+    time=$(date +%H%M)
+
+    if [[ "$time" -gt 1900 || "$time" -lt 0600 ]] && [[ $st != "GB News" ]]; then
+        timeout=170
+    fi
+
+    conky_timer "$timeout"
+}
+
+###############################################################################
+# MAIN
+###############################################################################
+
+check_top_of_hour
+
+mute_app
+show_muted_osd
+
+default_adbreak
+
+unmute_app
+show_osd
